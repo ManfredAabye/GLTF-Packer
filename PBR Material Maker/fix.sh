8c1c8de1e79bd5b8aa12b269af70e736970c659b
ManfredAabye <zainhofer@gmx.de>
2025-10-06 12:27:26 +0200

    Delete PBR Material Maker directory

--- PBR Material Maker/fix.sh
+++
@@ -1,3 +0,0 @@
-export GIT_COMMITTER_NAME="Captain Ai"
-export GIT_COMMITTER_EMAIL="extrude.ragu@proton.me"
-git rebase -i --root --rebase-merges -x "git commit --amend --author=\"Captain Ai <extrude.ragu@proton.me>\" -C HEAD"
